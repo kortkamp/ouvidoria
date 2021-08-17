39c031d28b16347cf04cc153b429558db7cba410
kortkamp <marcelusmedius@gmail.com>
2021-08-17 14:51:21 -0300

    feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, TitleArea, UserArea } from './styles';
+
+import megaphoneImg from '../../assets/megaphone.svg'
 
 const Header = (): JSX.Element => {
   
   return (
     <Container>
       <div>
-        <p>Sistema de Ouvidoria</p>
-      </div>
-      <div>
-        <p>não logado</p>
+        <TitleArea>
+          <img src={megaphoneImg} alt="Imagem de megafone" />
+          <span>Sistema de Ouvidoria</span>
+        </TitleArea>
+        <UserArea>
+          <p>não logado</p>
+        </UserArea>
       </div>
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,10 +1,34 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  font-size: 1.7rem;
-  height: 4rem;
-  width:100%;
-  display:flex;
-  justify-content:space-between;
+export const Container = styled.header`
+    height: 4rem;
+    width:100%;
+    background:var(--green);
+    color:var(--text-title);
+    
+    > div {
+      margin: 0 auto;
+      max-width:var(--page-width);
+      display:flex;
+      justify-content:space-between;
+      padding:0.5rem 0.5rem;
 
+      > div {
+        display: flex;
+        align-items:center;
+        gap:1rem;
+      }
+    }
+`;
+
+export const TitleArea = styled.div`
+  
+  font-size:1.8rem; 
+  img {
+    width:4rem;
+  }
+`;
+
+export const UserArea = styled.div`
+  
 `;
