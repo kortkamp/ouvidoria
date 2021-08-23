7e9610118ab54e6f0e3e146bba3f2c40c7fc646f
kortkamp <marcelusmedius@gmail.com>
2021-08-23 15:44:19 -0300

    feat: add district header style

--- src/pages/District/styles.ts
+++ src/pages/District/styles.ts
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  margin: 0 auto;
+  max-width:var(--page-width);
 
+  .districtHeader{
+    padding: 2.4rem 0;
+    h3 {
+      text-align:center;
+    }
+  }
 `;
