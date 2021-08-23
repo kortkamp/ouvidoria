19e7420c629cf0eabf45dc3548c5d0844705a853
kortkamp <marcelusmedius@gmail.com>
2021-08-23 08:18:24 -0300

    feat: District Page get data from API

--- src/pages/District/index.tsx
+++ src/pages/District/index.tsx
@@ -1,22 +1,68 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../../components/Header';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
 type DistrictParams = {
   id:string;
 }
 
+interface IAnswer {
+  id:string;
+  message:string;
+  deadline:number;
+  created_at:string;
+  user:{
+    name:string;
+    admin:boolean;
+  }
+}
+
+interface IComplaint {
+  id:string;
+  message:string;
+  image:string;
+  created_at:string;
+  user:{
+    name:string;
+    admin:boolean;
+  }
+  answers: IAnswer[];
+}
+
+interface IDistrict {
+  id:string;
+  name: string;
+}
+
 const District = (): JSX.Element => {
 
+  const [district,setDistrict] = useState<IDistrict>();
+  const [complaints, setComplaints] = useState<IComplaint[]>([]);
   const params = useParams<DistrictParams>();
-
   const districtId = params.id;
+
+  useEffect(()=>{
+    api.get(`/districts/${districtId}`)
+      .then((response) => {
+        
+        setDistrict(response.data);
+        setComplaints( response.data.complaints)});
+  },[districtId]);
+
 
   return (
     <>
       <Header />
       <Container>
-        <p>{districtId}</p>
+        <p>{district?.name}</p>
+        <ul>
+          {complaints.map((complaint)=>(
+            <li key={complaint.id} >{complaint.message}</li>
+          ))}  
+        </ul>
       </Container>
     </>
   )
