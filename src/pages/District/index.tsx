3fce6fcaaa9499349a04619374dfcd860a82daf4
kortkamp <marcelusmedius@gmail.com>
2021-08-24 22:48:58 -0300

    refactor: move logic to ComplaintsList

--- src/pages/District/index.tsx
+++ src/pages/District/index.tsx
@@ -10,29 +10,6 @@ type DistrictParams = {
   id:string;
 }
 
-interface IAnswer {
-  id:string;
-  message:string;
-  deadline:number;
-  created_at:string;
-  user:{
-    name:string;
-    admin:boolean;
-  }
-}
-
-interface IComplaint {
-  id:string;
-  message:string;
-  image:string;
-  created_at:string;
-  user:{
-    name:string;
-    admin:boolean;
-  }
-  answers: IAnswer[];
-}
-
 interface IDistrict {
   id:string;
   name: string;
@@ -41,16 +18,16 @@ interface IDistrict {
 const District = (): JSX.Element => {
 
   const [district,setDistrict] = useState<IDistrict>();
-  const [complaints, setComplaints] = useState<IComplaint[]>([]);
+  
   const params = useParams<DistrictParams>();
   const districtId = params.id;
 
   useEffect(()=>{
     api.get(`/districts/${districtId}`)
       .then((response) => {
-        
         setDistrict(response.data);
-        setComplaints( response.data.complaints)});
+      }
+    );
   },[districtId]);
 
 
@@ -61,7 +38,7 @@ const District = (): JSX.Element => {
         <div className='districtHeader'>
           <h3>Bairro {district?.name}</h3>
         </div>
-        <ComplaintsList complaints={complaints}/>
+        <ComplaintsList districtId={districtId}/>
       </Container>
     </>
   )
