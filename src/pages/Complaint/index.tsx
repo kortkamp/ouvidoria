6179f5d8d14774510a5c0688d144e616eaa52f10
kortkamp <marcelusmedius@gmail.com>
2021-08-25 22:27:46 -0300

    feat: add complaint page

---
+++ src/pages/Complaint/index.tsx
@@ -0,0 +1,21 @@
+import Header from '../../components/Header';
+import {Container} from './styles'
+
+
+const Complaint = (): JSX.Element => {
+
+  return(
+    <>
+      <Header />
+      <Container>
+        complaint
+      </Container>
+    </>
+
+  )
+}
+
+export default Complaint;
+
+
+
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import District from './pages/District';
+import Complaint from './pages/Complaint'
 
 const Routes = (): JSX.Element => {
   return (
     <Switch>
       <Route path='/' exact component={ Home } />
       <Route path='/district/:id' component={ District }/>
+      <Route path='/complaint' component={ Complaint } />
     </Switch>
   );
 };
